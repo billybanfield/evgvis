69e89b8c38d92bbe6590667b2135fec5e0c18d25
Will Banfield <william.banfield@mongodb.com>
2016-06-25 11:53:11 -0400

    CHANGES

--- main.go
+++ main.go
@@ -2,4 +2,15 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
+	"net/http"
 )
+
+func hello(c *gin.Context) {
+	c.String(http.StatusOK, "hello")
+}
+
+func main() {
+	router := gin.New()
+	router.GET("/", hello)
+	router.Run(":8000")
+}
